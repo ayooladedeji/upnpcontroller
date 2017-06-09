7a13cb01e5bfec45edec4743e84f07431777938e
Ayo Adedeji <Ayo@Ayos-MacBook-Pro.local>
2017-06-09 16:54:04 +0100

    added navigation drawer where list of devices will go

--- app/src/main/java/com/cambridgeaudio/upnpcontroller/MainActivity.java
+++ app/src/main/java/com/cambridgeaudio/upnpcontroller/MainActivity.java
@@ -11,24 +11,44 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
+        setUpDrawerLayout();
+
+
+    }
+
+    private void setUpDrawerLayout(){
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        getMediaServers();
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void getMediaServers(){
+        Menu menu = navigationView.getMenu();
+        for (int i = 1; i <= 3; i++) {
+            menu.add("Media Server " + i);
+        }
     }
 
     @Override
